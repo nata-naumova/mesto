20c991424e3c2e21305da764c4b2b885663a17b7
Nata Nau <naumonata@yandex.ru>
2022-04-26 18:10:48 +0300

    исправила ошибки в js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 /* Объявления переменных */
 const profile = document.querySelector('.profile');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 const editBtn = profile.querySelector('.profile__edit-btn');
 
 const popup = document.querySelector('.popup');
@@ -11,37 +11,31 @@ const formElement = popup.querySelector('.popup__form');
 let nameInput = formElement.querySelector('#popup__input_name');
 let jobInput = formElement.querySelector('#popup__input_job');
 
-const saveBtn = popup.querySelector('.popup__btn');
-
-/* Функция открытия формы */
+/* 
+Функция открытия формы, должна изменять 
+значения в форме из значений в профиле
+*/
 function openedForm() {
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
+
     popup.classList.add('popup_opened');
 }
 
 /* Функция закрытия формы */
 function closeForm() {
-    let titleName = profileTitle.textContent.length;
-    let titleInput = nameInput.value.length;
-    let subtitleJob = profileSubtitle.textContent.length;
-    let subtitleInput = jobInput.value.length;
-
     popup.classList.remove('popup_opened');
-
-    if(titleName != titleInput) {
-        nameInput.value = profileTitle.textContent;
-    } else if(subtitleJob != subtitleInput) {
-        jobInput.value = profileSubtitle.textContent;
-    }
 }
 
 /* Функция обработчик "отправки" формы */
 function formSubmitHandler(evt) {
     /* Отменить стандартную отправку */
     evt.preventDefault(); 
-    
+
     /* Добавления новых значений */
-    profileTitle.textContent = nameInput.value;
-    profileSubtitle.textContent = jobInput.value;
+    profileTitle.textContent = nameInput.value; 
+    profileSubtitle.textContent = jobInput.value; 
+
     closeForm();
 }
 
