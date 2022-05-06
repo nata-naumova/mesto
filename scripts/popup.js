25e12a97f954f48ee46f6478114f53b89d587c03
Nata Nau <naumonata@yandex.ru>
2022-05-07 00:26:29 +0300

    Реализовала логику карточек

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,42 +1,103 @@
-/* 1. КАРТОЧКИ */
 const cardList = document.querySelector('.elements');
-const cardTemplate = document.querySelector('.element-template').content;
+const cardForm = document.querySelector('.add-card');
+const template = document.querySelector('.element-template');
 
 const initialCards = [
     {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+        name: 'Пантера в папоротнике',
+        link: 'https://catherineasquithgallery.com/uploads/posts/2021-02/1612582013_50-p-foto-zhivotnikh-na-zelenom-fone-77.jpg'
     },
     {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        name: 'Снежный Барс',
+        link: 'https://proprikol.ru/wp-content/uploads/2020/12/snezhnye-barsy-krasivye-kartinki-11.jpg'
+    },
+    {
+        name: 'Сервал Мяурицио',
+        link: 'https://wallpapershome.ru/images/pages/pic_h/1336.jpg'
+    },
+    {
+        name: 'Лесной Кот',
+        link: 'https://cdn.fishki.net/upload/post/2021/01/27/3576227/eb08e205c4865e5f6d6313b307c10c5c.jpg'
+    },
+    {
+        name: 'Енотовидная панда',
+        link: 'https://www.ejin.ru/wp-content/uploads/2017/11/24-5.jpg'
+    },
+    {
+        name: 'Енот-полоскун',
+        link: 'https://wallbox.ru/resize/960x800/wallpapers/main/201523/85121fb83f62a3c.jpg'
     }
 ];
 
-initialCards.forEach(function (el) {
-    const cardItem = cardTemplate.cloneNode(true);
-    cardItem.querySelector('.element__img').src = el.link;
-    cardItem.querySelector('.element__img').alt = el.name;
-    cardItem.querySelector('.element__title').textContent = el.name;
+const handleDeleteCard = (evt) => {
+    evt.target.closest('.element').remove();
+};
+
+const handleLikeCard = (evt) => {
+    const like = evt.target.closest('.element__btn');
+    like.classList.toggle('element__btn_active');
+};
+
+const getCardElement = (cardName, cardLink) => {
+    const newCardElement = template.content.cloneNode(true);
+    const newCardName = newCardElement.querySelector('.element__title');
+    const newCardLink = newCardElement.querySelector('.element__img');
+
+    newCardName.textContent = cardName;
+    newCardLink.src = cardLink;
+    newCardLink.alt = cardName;
+        
+    const deleteButton = newCardElement.querySelector('.element__trash');
+    const likeButton = newCardElement.querySelector('.element__btn');
+
+    deleteButton.addEventListener('click', handleDeleteCard);
+    likeButton.addEventListener('click', handleLikeCard);
     
-    cardList.append(cardItem);
+    return newCardElement;
+}
+
+cardForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const name = evt.target.nameCard.value;
+    const link = evt.target.linkCard.value;
+
+    if(name && link) {
+      renderCard(cardList, name, link);  
+    }
+    evt.target.reset();
+    const targetClose = evt.target.closest('.popup');
+    popupClosed(targetClose);
+})
+
+const renderCard = (wrap, cardName, cardLink) => {
+    wrap.prepend(getCardElement(cardName, cardLink));
+}
+
+initialCards.forEach((item) => {
+    const cardName = item.name;
+    const cardLink = item.link;
+    renderCard(cardList, cardName, cardLink);
 });
 
-/* 2. ФОРМА ДОБАВЛЕНИЯ КАРТОЧКИ */
+
 const popupLinks = document.querySelectorAll('.popup-link');
 const popupClose = document.querySelectorAll('.popup__close');
-
 
 if(popupLinks.length > 0) {
     for(let i = 0; i < popupLinks.length; i++) {
         const popupLink = popupLinks[i];
-        
         popupLink.addEventListener('click', function (e) {
-            //console.log('1');
             const popupName = popupLink.dataset.dismiss;
             const currentPopup = document.getElementById(popupName);
             popupOpen(currentPopup);
             e.preventDefault();
+            
+            const popupImg = document.querySelector('.popup__img');
+            const popupTitleImg = document.querySelector('.popup__img-title');
+            const popupLinkImg = e.target.closest('.element__img-wrapper').querySelector('.element__img');
+            popupImg.src = popupLinkImg.src;
+            popupImg.alt = popupLinkImg.alt;
+            popupTitleImg.textContent = popupLinkImg.alt;
         });
     }
 }
@@ -61,42 +122,4 @@ function popupClosed(targetClose) {
 }
 
 
-/* 3. ДОБАВЛЕНИЕ НОВОЙ КАРТОЧКИ - unshift */
-
-/* 4. КАРТОЧКИ ЛАЙКИ */
-const cardLike = document.querySelectorAll('.element__btn');
-
-if(cardLike.length > 0) {
-    for(let i = 0; i < cardLike.length; i++) {
-        const like = cardLike[i];
-        like.addEventListener('click', function (e) {
-            addLike(like);
-            e.preventDefault();
-        });
-    }
-}
-
-function addLike(like) {
-    like.classList.toggle('element__btn_active');
-}
-
-/* 5. УДАЛЕНИЕ КАРТОЧКИ - splice */
-const cardTrash = document.querySelectorAll('.element__trash');
-
-if(cardTrash.length > 0) {
-    for(let i = 0; i < cardTrash.length; i++) {
-        const trash = cardTrash[i];
-        trash.addEventListener('click', function (e) {
-            cardTrashed(trash);
-            e.preventDefault();
-        });
-    }
-}
-
-function cardTrashed(trash) {
-    initialCards.splice(1,1);
-}
-
-//document.addEventListener('click',e => console.log(e.target))
-
-/* 6. ОТКРЫТИЕ ПОПАПА С КАРТИНКОЙ */
+//document.addEventListener('click',e => console.log(e.target));
